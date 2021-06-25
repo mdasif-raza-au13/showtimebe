b2eb9f011f29ec52b72289b0a3dc206277c0ca7c
asif <mdasifr28@gmail.com>
2021-06-25 19:53:58 +0530

    backend added

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import bookingapi from './routes/bookingapi.js'
-import userapi from './routes/userapi.js'
-import adminapi from './routes/adminapi.js'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+var authRouter = require("./routes/auth");
+var SignupRouter = require("./routes/usersignup");
+// Post = require("./models/model.post");
+const apiRouter = require("./routes/api.router");
+city1 = require("./models/sample");
+console.log(city1);
+//cors
+app.use(cors());
 
-const app = express()
-const port = process.env.PORT || 5000
+//bodyparser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+//Db Connection
+const db = require("./config/db.config");
+db.authenticate()
+  .then(() => {
+    console.log(" Db connected Successfully!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+//Routes
 
-mongoose.connect(process.env.url,{useNewUrlParser: true, useUnifiedTopology: true})
-const db = mongoose.connection
-db.on('error',(error) => console.log(error))
-db.once('open',() => console.log('database connected'));
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
+app.use("/userSignup", SignupRouter);
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/api',bookingapi)
-app.use('/user',userapi)
-app.use('/admin',adminapi)
-app.get('/',(req, res) => {
-    res.send('Route is perfect')
-})
-app.all('*', (req, res) => {
-    res.send('Error 404 Page not found')
-})
-
-app.listen(port, () => {console.log(`http://localhost:${port}`)})
+//Port
+const port = process.env.PORT || 5000;
+const ENV = process.env.NODE_ENV;
+if (ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "./client/bookmyticket_client/build"))
+  );
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "./client/bookmyticket_client/build/index.html")
+    );
+  });
+}
+app.listen(port, () => {
+  console.log(`Runnning on ${port}`);
+});
--- middleware/validation.js
+++
@@ -1,4 +0,0 @@
-import express from 'express'
-import Users from '../models/Users'
-import { body, validationResult } from 'express-validator'
-
